91f852363a0b757444b7a7f07c05c90db4995f2e
lapmach <lapmach7@gmail.com>
2024-05-08 21:05:24 +0700

    xinchao3

---
+++ React/ES6_Phan2.js
@@ -0,0 +1,34 @@
+
+
+let snt = (x) => {
+    if(x < 2)
+        return false;
+
+    for(let i=2 ; i<x-1 ;i++){
+        if(x%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(snt(7));
+console.log("==========^^^==========");
+
+
+let arr = [1,5,9,8,6,7,101];
+arr.map(x =>{
+    if(snt(x) == true){
+        console.log(x);
+    }
+})
+console.log("==========^^^==========");
+
+let mangMoi = arr.filter(item => {
+    if(snt(item) == true){
+        console.log(item);
+    }
+})
+console.log("==========^^^==========");
+let sumReduce = arr.reduce((a , x) => a + x*x,0);
+console.log(sumReduce);
